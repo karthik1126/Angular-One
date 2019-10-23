b20bcab4410126db2ec2c74d9354a5d494ee9821
dgit999 <kthirun@upcontractor.up.com>
2019-10-23 01:42:33 -0500

    update..day2

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,30 +2,43 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '.app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .colorWhite {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent {
-  serverID = 123;
+  serverID = 0;
   serverName = "Intel Pentium";
   allowToAddServer = false;
   serverCreationStatus = "No Server Created";
+  isServerCreated = false;
+  servers = [];
 
   getServerName() {
     return this.serverName;
   }
 
   addServer() {
+    this.serverID = Math.random();
     this.serverCreationStatus = "Server got created with server ID : " + this.serverID + " and server name : " + this.serverName;
+    this.servers.push(this.serverCreationStatus);
   }
 
   enterServerID(event : Event) {
-    console.log(event);
-    this.serverID = (<HTMLInputElement>event.target).value;
+     this.isServerCreated = true;
+    this.serverID = Number.parseInt((<HTMLInputElement>event.target).value);
   }
 
   constructor() {
     setTimeout(() => {
       this.allowToAddServer = true;
     }, 1000)
+  }
+
+  getStyle() {
+    return this.serverID > 0.5? 'green': 'red';
   }
 }
